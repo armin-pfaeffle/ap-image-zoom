63fad57a18a771278d0920071fc15c7e75a81ace
Armin <mail@armin-pfaeffle.de>
2014-09-17 22:16:21 +0200

    - Improved plugin so it can be called from a container with imageUrl parameter - Removed adding unnecessary ID - Added prefix "apiz" for events, e.g. "apizInit" - Make events camelCase, for better readability - Renamed option "initialSize" to "initialZoom" - New feature: option initialZoom now can be a float which defines the   initial zoom

--- js/ap-image-zoom.js
+++ js/ap-image-zoom.js
@@ -1,6 +1,6 @@
 /**
-* @license ap-image-zoom.js v0.6
-* Updated: 16.09.2014
+* @license ap-image-zoom.js v0.7
+* Updated: 17.09.2014
 * {DESCRIPTION}
 * Copyright (c) 2014 armin pfaeffle
 * Released under the MIT license
@@ -11,6 +11,7 @@
 
 	var datakey = '__apiz__';
 	var cssPrefix = 'apiz-';
+	var eventPrefix = 'apiz';
 
 	/**
 	 * Makes the first character of str uppercase and returns that string.
@@ -30,6 +31,13 @@
 			return ucfirst(this);
 		};
 	}
+
+	/**
+	 *
+	 */
+	jQuery.fn.tagName = function() {
+		return this.prop("tagName").toLowerCase();
+	};
 
 	/**
 	 * 2D Point class with properties x and y. Parses x and y to Float and applies
@@ -62,19 +70,24 @@
 	/**
 	 * Constructor for ApImageZoom plugin.
 	 */
-	function ApImageZoom(image, options) {
+	function ApImageZoom(element, options) {
 		// Do not remake the zoom plugin
-		var data = $(image).data(datakey);
+		var data = $(element).data(datakey);
 		if (data) {
 			return data;
 		}
 
-		this.$image = $(image);
 		this.settings = $.extend({}, ApImageZoom.defaultSettings, options);
+		this.$target = $(element);
+		this.mode = (this.$target.tagName() == 'img' ? 'image' : 'container');
+		this.imageUrl = (this.mode == 'image' ? this.$target.attr('src') : this.settings.imageUrl);
 		this._init();
 
 		// Save the instance
-		this.$image.data(datakey, this);
+		this.$target.data(datakey, this);
+		if (this.mode == 'container') {
+			this.$image.data(datakey, this);
+		}
 	}
 
 	/**
@@ -91,40 +104,32 @@
 			this.panning = false;
 			this.pinching = false;
 
-			// Store original size and style for restoring on destruction
-			this.originSize = new Size(this.$image.width(), this.$image.height());
-			this.originStyle = this.$image.attr('style');
-			this.$image.removeAttr('style');
-
-			// Place image into wrapper and hide it unless we have the natural size
-			this._wrapImage();
+			this._addWrapper();
 			this._setCssClasses();
 
+			if (!this.imageUrl) {
+				this._showError('Invalid image url!');
+			}
+
 			// Create a temporary hidden copy of image, so we obtain the real/natural size
-			$('<img />')
+			this.$image = $('<img />')
 				.hide()
-				.appendTo('body')
+				.prependTo(this.$wrapper)
 				.load(function() {
-					self.naturalSize = new Size($(this).width(), $(this).height());
+					self._obtainImageSizes();
 					self._setConstraints();
 					self._setup();
-
-					// Remove temporary image
-					$(this).remove();
 				})
 				.error(function() {
 					self._showError('Error loading image!');
-					if (self.$loadingAnimation) {
-						self.$loadingAnimation.remove();
-					}
 				})
-				.attr('src', this.$image.attr('src'));
+				.attr('src', this.imageUrl);
 		},
 
 		/**
 		 *
 		 */
-		_wrapImage: function() {
+		_addWrapper: function() {
 			// Setup wrapper and overlay
 			this.$wrapper = $('<div></div>').addClass(cssPrefix + 'wrapper');
 			this.$overlay = $('<div></div>').addClass(cssPrefix + 'overlay');
@@ -133,17 +138,27 @@
 			// Add loading text/throbber
 			this._addLoadingAnimation();
 
-			// Apply ID with prefix "apiz-" if image has one
-			var id = this.$image.attr('id');
-			if (id) {
-				this.$wrapper.attr('id', cssPrefix + id);
+			// Hide image and move it into added wrapper or add wrapper target container
+			if (this.mode == 'image') {
+				this.imageIsVisible = this.$target.is(':visible');
+				this.$target.hide().after(this.$wrapper).appendTo(this.$wrapper);
 			}
+			else {
+				this.$wrapper.appendTo(this.$target);
+			}
+		},
 
-			// Hide image and place it into the wrapper
-			this.$image
-				.hide()
-				.after(this.$wrapper)
-				.prependTo(this.$wrapper);
+		/**
+		 *
+		 */
+		_removeWrapper: function() {
+			if (this.mode == 'image') {
+				this.$wrapper.after(this.$target);
+				if (this.imageIsVisible) {
+					this.$target.show();
+				}
+			}
+			this.$wrapper.remove();
 		},
 
 		/**
@@ -180,6 +195,15 @@
 		/**
 		 *
 		 */
+		_obtainImageSizes: function() {
+			var $image = (this.mode == 'image' ? this.$target : this.$image);
+			this.originSize = new Size($image.width(), $image.height());
+			this.naturalSize = new Size(this.$image.width(), this.$image.height());
+		},
+
+		/**
+		 *
+		 */
 		_setup: function() {
 			this._resetSize();
 			this._center();
@@ -199,19 +223,11 @@
 		/**
 		 *
 		 */
-		_unwrap: function() {
-			this.$wrapper
-				.after(this.$image)
-				.remove();
-		},
-
-		/**
-		 *
-		 */
 		_setCssClasses: function() {
 			if (typeof this.settings.cssWrapperClass == 'string') {
 				this.$wrapper.addClass(this.settings.cssWrapperClass);
 			}
+			this.$wrapper.addClass(cssPrefix + 'mode-' + this.mode);
 			var cssClasses = {
 				hammer: {
 					status: !this.settings.disableHammerPlugin,
@@ -273,7 +289,7 @@
 
 			// MouseWheel: zoom
 			if (!this.settings.disableMouseWheelPlugin && typeof jQuery.fn.mousewheel == "function") {
-				this.$overlay.on('mousewheel', function(evt) { self._onMouseWheel(evt); } );
+				this.$overlay.on('mousewheel.apimagezoom', function(evt) { self._onMouseWheel(evt); } );
 			}
 		},
 
@@ -287,7 +303,7 @@
 				this._isPanning(false);
 				this._isPinching(false);
 			}
-			this.$overlay.unbind('mousewheel');
+			this.$overlay.off('mousewheel.apimagezoom');
 		},
 
 		/**
@@ -304,12 +320,12 @@
 					imagePosition: this._imagePosition(),
 					cursorPosition: new Point(evt.pointers[0].screenX, evt.pointers[0].screenY)
 				};
-				this._trigger('panstart', [this.panStart.imagePosition]);
+				this._trigger('panStart', [this.panStart.imagePosition]);
 			}
 			else if (evt.type == 'panend') {
 				this._isPanning(false);
 				this.$wrapper.removeClass(cssPrefix + 'is-panning');
-				this._trigger('panend', [this._imagePosition()]);
+				this._trigger('panEnd', [this._imagePosition()]);
 			}
 			else {
 				var position = new Point(
@@ -317,7 +333,7 @@
 					this.panStart.imagePosition.y + (evt.pointers[0].screenY - this.panStart.cursorPosition.y)
 				);
 				var updatedPosition = this._move(position);
-				this._trigger('panmove', [updatedPosition]);
+				this._trigger('panMove', [updatedPosition]);
 			}
 			evt.preventDefault();
 		},
@@ -349,11 +365,11 @@
 					imagePosition: this._imagePosition(),
 					relativeOrigin: relativeOrigin
 				};
-				this._trigger('pinchstart', [imageSize, this.pinchStart.imagePosition]);
+				this._trigger('pinchStart', [imageSize, this.pinchStart.imagePosition]);
 			}
 			else if (evt.type == 'pinchend') {
 				this._isPinching(false);
-				this._trigger('pinchend', [this._imageSize(), this._imagePosition()]);
+				this._trigger('pinchEnd', [this._imageSize(), this._imagePosition()]);
 			}
 			else {
 				// Here we do NOT depend on the internal zoomTo method because while
@@ -375,7 +391,7 @@
 						this.pinchStart.imagePosition.y - (deltaHeight * this.pinchStart.relativeOrigin.y)
 					);
 					var updatedPosition = this._move(position);
-					this._trigger('pinchmove', [updatedSize, updatedPosition]);
+					this._trigger('pinchMove', [updatedSize, updatedPosition]);
 				}
 			}
 			evt.preventDefault();
@@ -392,10 +408,10 @@
 			// Only trigger event
 			var eventType;
 			switch (evt.direction) {
-				case  8: eventType = 'swipetop'; break;
-				case  4: eventType = 'swiperight'; break;
-				case 16: eventType = 'swipebottom'; break;
-				case  2: eventType = 'swipeleft'; break;
+				case  8: eventType = 'swipeTop'; break;
+				case  4: eventType = 'swipeRight'; break;
+				case 16: eventType = 'swipeBottom'; break;
+				case  2: eventType = 'swipeLeft'; break;
 			};
 			if (eventType) {
 				this._trigger(eventType, [evt]);
@@ -429,8 +445,7 @@
 			var imageSize = this._imageSize();
 			switch (this.settings.doubleTap) {
 				case 'open':
-					var src = this.$image.attr('src');
-					window.open(src);
+					window.open(this.imageUrl);
 					break;
 
 				case 'zoomMax':
@@ -455,7 +470,7 @@
 				this._zoomTo(zoom, origin);
 			}
 
-			this._trigger('doubletap', [evt]);
+			this._trigger('doubleTap', [evt]);
 			evt.preventDefault();
 		},
 
@@ -468,7 +483,6 @@
 			}
 
 			var zoom = this._getZoom() + (this.settings.zoomStep * evt.deltaY);
-
 			var imageSize = this._imageSize();
 			var origin = new Scale(
 				(evt.pageX - this.$image.offset().left) / imageSize.width,
@@ -476,7 +490,7 @@
 			);
 			var sizeAndPosition = this._zoomTo(zoom, origin);
 
-			this._trigger('mousewheel', [sizeAndPosition.size, sizeAndPosition.position]);
+			this._trigger('mouseWheel', [sizeAndPosition.size, sizeAndPosition.position]);
 			evt.preventDefault();
 		},
 
@@ -500,9 +514,14 @@
 		 *
 		 */
 		_resetSize: function() {
-			var size = this.originSize;
-			if (typeof this.settings.initialSize == 'string') {
-				switch (this.settings.initialSize) {
+			var updatedSize;
+			if (typeof this.settings.initialZoom == 'float' || this.settings.initialZoom == parseFloat(this.settings.initialZoom)) {
+				var zoom = parseFloat(this.settings.initialZoom);
+				this._zoomTo(zoom);
+			}
+			else if (typeof this.settings.initialZoom == 'string') {
+				var size = this.originSize;
+				switch (this.settings.initialZoom) {
 					case "auto":
 						size = this._getAutoFitSize();
 						break;
@@ -515,8 +534,9 @@
 						size = new Size(this.sizeConstraints.width.max, this.sizeConstraints.height.max);
 						break;
 				}
+				updatedSize = this._resize(size);
 			}
-			var updatedSize = this._resize(size);
+			updatedSize = updatedSize || this._imageSize();
 			this._trigger('resetSize', [updatedSize]);
 		},
 
@@ -740,9 +760,10 @@
 			var optionName = 'on' + eventType.ucfirst();
 			if (typeof this.settings[optionName] == 'function') {
 				var f = this.settings[optionName];
-				f.apply(this.$image, args);
+				f.apply(this.$target, args);
 			}
-			this.$image.trigger(eventType, args);
+			eventType = eventPrefix + eventType.ucfirst();
+			this.$target.trigger(eventType, args);
 		},
 
 		/**
@@ -754,9 +775,10 @@
 				result;
 			if (typeof this.settings[optionName] == 'function') {
 				var f = this.settings[optionName];
-				callbackResult = f.apply(this.$image, args);
+				callbackResult = f.apply(this.$target, args);
 			}
-			result = ((result = this.$image.triggerHandler(eventType, args)) !== undefined ? result : callbackResult);
+			eventType = eventPrefix + eventType.ucfirst();
+			result = ((result = this.$target.triggerHandler(eventType, args)) !== undefined ? result : callbackResult);
 			return result;
 		},
 
@@ -767,6 +789,9 @@
 			if (!this.$errorMessage) {
 				this.$errorMessage = $('<div></div>').addClass(cssPrefix + 'error');
 				this.$wrapper.append(this.$errorMessage);
+			}
+			if (this.$loadingAnimation) {
+				this.$loadingAnimation.remove();
 			}
 			this.$errorMessage.html(message);
 		},
@@ -963,21 +988,18 @@
 		 *
 		 */
 		destroy: function() {
-			this.$image.trigger('destroy');
+			this._trigger('destroy');
 
 			this._unbind();
-			this._unwrap();
-
-			// When reseting style it's important to remove style first and then set it again
-			// because this.originStyle can be undefined and if so only settings style won't
-			// do the job correctly
-			this.$image.removeAttr('style').attr('style', this.originStyle);
-			this._imageSize(this.originSize);
+			this._removeWrapper();
 
 			this._isPanning(false);
 			this._isPinching(false);
 
-			this.$image.removeData(datakey);
+			this.$target.removeData(datakey);
+			if (this.mode == 'container') {
+				this.$image.removeData(datakey);
+			}
 		}
 	};
 
@@ -1015,6 +1037,16 @@
 	 * Default settings for ApZoomImage plugin.
 	 */
 	ApImageZoom.defaultSettings = {
+		imageUrl: undefined,
+		loadingAnimation: undefined,	// Options: undefined, 'text', 'throbber', 'image'
+		loadingAnimationData: undefined,
+		cssWrapperClass: undefined,
+		initialZoom: 'auto',			// Options: value (float), 'none', 'auto', 'min', 'max'
+		minZoom: 0.2,					// = 20%
+		maxZoom: 1.0,					// = 100%
+		zoomStep: 0.1,					// = 10% steps
+		autoCenter : true,				// Options: true, 'both', 'horizontal', 'vertical'
+
 		disableHammerPlugin: false,
 		disableMouseWheelPlugin: false,
 
@@ -1022,21 +1054,7 @@
 		disablePan: false,
 		disableZoom: false,
 
-		loadingAnimation: undefined,	// Options: undefined, 'text', 'throbber', 'image'
-		loadingAnimationData: undefined,
-		initialSize: 'auto',			// Options: 'none', 'auto', 'min', 'max'
-		cssWrapperClass: undefined,
-		minZoom: 0.2,					// = 20%
-		maxZoom: 1.0,					// = 100%
-		zoomStep: 0.1,					// = 10% steps
-		autoCenter : true,				// Options: true, 'both', 'horizontal', 'vertical'
-
-		doubleTap: undefined,			// Options: 'open', 'zoomMax', 'zoomToggle'
-
-		onBeforeSizeChange: undefined,
-		onSizeChanged: undefined,
-		onBeforePositionChange: undefined,
-		onPositionChanged: undefined
+		doubleTap: undefined			// Options: 'open', 'zoomMax', 'zoomToggle'
 	};
 
 }(jQuery));
